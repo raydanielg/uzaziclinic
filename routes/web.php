34a2d9cf85c97beab54845b2b5d958e7aa715632
raydanielg <airezra2+1@gmail.com>
2026-05-10 02:13:05 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -153,11 +153,13 @@ Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(fun
     });
 
     // E-commerce / Store
-    Route::prefix('store')->name('products.')->group(function () {
+    Route::prefix('store')->name('store.')->group(function () {
         Route::get('/', [App\Http\Controllers\Admin\EcommerceController::class, 'index'])->name('index');
         Route::get('/create', [App\Http\Controllers\Admin\EcommerceController::class, 'create'])->name('create');
         Route::post('/store', [App\Http\Controllers\Admin\EcommerceController::class, 'store'])->name('store');
         Route::get('/orders', [App\Http\Controllers\Admin\EcommerceController::class, 'orders'])->name('orders');
+        Route::get('/categories', [App\Http\Controllers\Admin\EcommerceController::class, 'categories'])->name('categories');
+        Route::get('/reviews', [App\Http\Controllers\Admin\EcommerceController::class, 'reviews'])->name('reviews');
     });
 
     // Payments & Billing
