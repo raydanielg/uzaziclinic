9aa4ea0f90aa66a13d80f5850868e1f5c2d0b28b
raydanielg <airezra2+1@gmail.com>
2026-05-10 02:14:59 +0300

    commit

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,8 +11,43 @@ class Role extends Model
 
     protected $fillable = ['name', 'permissions'];
 
+    protected $casts = [
+        'permissions' => 'array',
+    ];
+
     public function users()
     {
         return $this->hasMany(User::class);
+    }
+
+    /**
+     * Check if role has a specific permission
+     */
+    public function hasPermission($permission)
+    {
+        $permissions = is_string($this->permissions) 
+            ? json_decode($this->permissions, true) 
+            : $this->permissions;
+
+        if (!$permissions) {
+            return false;
+        }
+
+        // Admin has all permissions
+        if (isset($permissions['all_access']) || isset($permissions['all'])) {
+            return true;
+        }
+
+        return isset($permissions[$permission]) && $permissions[$permission] === true;
+    }
+
+    /**
+     * Get all permissions as array
+     */
+    public function getPermissionsArray()
+    {
+        return is_string($this->permissions) 
+            ? json_decode($this->permissions, true) 
+            : ($this->permissions ?? []);
     }
 }
