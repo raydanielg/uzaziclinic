c6510ebe0021e3999e6371fe284859c440ffb946
raydanielg <airezra2+1@gmail.com>
2026-05-10 14:38:15 +0300

    commit

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class NotificationController extends Controller
 {
     public function send() { return view('admin.notifications.send'); }
-    public function history() { return view('admin.notifications.send'); }
-    public function emailTemplates() { return view('admin.notifications.send'); }
-    public function smsTemplates() { return view('admin.notifications.send'); }
+    public function history() { return view('admin.notifications.history'); }
+    public function emailTemplates() { return view('admin.notifications.email_templates'); }
+    public function smsTemplates() { return view('admin.notifications.sms_templates'); }
 }
