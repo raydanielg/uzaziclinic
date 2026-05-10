e6a787d4d9efc559d36a7212ba3b263a1d2c24f2
raydanielg <airezra2+1@gmail.com>
2026-05-10 15:26:37 +0300

    commit

---
+++ database/migrations/2026_05_10_122631_create_lab_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lab_requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lab_requests');
+    }
+}
