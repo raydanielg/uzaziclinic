51b4cc97b3a9e47b57b9111297ea31dc0ca7cb75
raydanielg <airezra2+1@gmail.com>
2026-05-30 00:24:44 +0300

    commit

--- database/migrations/2026_05_05_194509_create_doctors_table.php
+++ database/migrations/2026_05_05_194509_create_doctors_table.php
@@ -17,7 +17,7 @@ class CreateDoctorsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('specialization', 100)->nullable();
-            $table->string('license_number', 50)->unique();
+            $table->string('license_number', 50)->nullable()->unique();
             $table->text('bio')->nullable();
             $table->timestamps();
         });
