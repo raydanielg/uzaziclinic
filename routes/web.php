eb9b5d5b6c80e525c62200e60839c9a550e295f4
raydanielg <airezra2+1@gmail.com>
2026-05-29 11:58:57 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -207,6 +207,7 @@ Route::middleware(['auth', 'role:customer'])->prefix('patient')->name('patient.'
 
 Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\Admin\DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('services', App\Http\Controllers\Admin\ServiceController::class);
     Route::get('/analytics', [App\Http\Controllers\Admin\AnalyticsController::class, 'index'])->name('analytics');
 
     // User Management
