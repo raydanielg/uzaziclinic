7b2f1b88255feae5f8ae580642be9007921ceedc
raydanielg <airezra2+1@gmail.com>
2026-06-01 08:30:51 +0300

    commit

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -46,4 +46,9 @@ class Doctor extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function appointments()
+    {
+        return $this->hasMany(Appointment::class);
+    }
 }
