7328d710b7ade68865351145a2c95816786c994e
raydanielg <airezra2+1@gmail.com>
2026-06-23 00:12:51 +0300

    commit

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -25,7 +25,7 @@ class RoleSeeder extends Seeder
         ];
 
         foreach ($roles as $role) {
-            Role::create($role);
+            Role::firstOrCreate(['name' => $role['name']], $role);
         }
     }
 }
